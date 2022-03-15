03ae04caee5dcfdb10e307491b1bbf6a70f53fe2
Sarah Lopez <slopez323@gmail.com>
2022-03-15 09:42:49 -0400

    next day refresh

--- main.js
+++ main.js
@@ -441,15 +441,15 @@ function showStatPopup() {
     $('.popup-stats').show();
 };
 
-let activeTime = new Date().getTime();
-$(document.body).bind("mousemove keypress touchstart", function () {
-    activeTime = new Date().getTime();
-});
+// let activeTime = new Date().getTime();
+// $(document.body).bind("mousemove keypress touchstart", function () {
+//     activeTime = new Date().getTime();
+// });
 
 setInterval(function() {
-    if (new Date().getTime() - activeTime >= 300000) {
-        window.location.reload(true);
-    }
+    // if (new Date().getTime() - activeTime >= 300000) {
+    //     window.location.reload(true);
+    // }
     if (localStorage.getItem(played.LAST_PLAYED) !== getTodaysDt()){
         window.location.reload(true);
     }
